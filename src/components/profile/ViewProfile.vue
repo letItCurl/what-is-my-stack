5578a299def0be447b161c2715d7368af64015a1
rollpz <rolandlopez.developer@gmail.com>
2020-03-13 19:04:12 +0100

    Comment are live !

--- src/components/profile/ViewProfile.vue
+++ src/components/profile/ViewProfile.vue
@@ -3,7 +3,10 @@
         <div v-if="profile" class="card">
             <h2 class="deep-purple-text center">{{profile.alias}}'s Wall</h2>
             <ul class="comments collection">
-                <li>comment</li>
+                <li v-for="(comment, index) in comments" :key="index">
+                    <div class="deep-purple-text">{{comment.from}}</div>
+                    <div class="grey-text text-darken-2">{{comment.content}}</div>
+                </li>
             </ul>
             <form @submit.prevent="addComment">
                 <div class="field">
@@ -27,12 +30,14 @@ export default {
             profile: null,
             feedback: null,
             newComment: null,
-            user: null
+            user: null,
+            comments: []
         }
     },
     created(){
         let ref = db.collection('users')
 
+        // get current user
         ref.where('user_id', '==', firebase.auth().currentUser.uid).get().then(snapshot => {
             snapshot.forEach(doc =>{
                 this.user = doc.data()
@@ -40,9 +45,23 @@ export default {
             })
         })
 
+        // profile data
         ref.doc(this.$route.params.id).get()
         .then(user => {
             this.profile = user.data()
+        })
+
+        // comments
+        db.collection('comments').where('to', '==', this.$route.params.id)
+        .onSnapshot((snapshot) => {
+            snapshot.docChanges().forEach(change => {
+                if(change.type == "added"){
+                    this.comments.unshift({
+                        from: change.doc.data().from,
+                        content: change.doc.data().content,
+                    })
+                }
+            })
         })
     },
     methods: {
@@ -51,7 +70,7 @@ export default {
                 this.feedback = null
                 db.collection("comments").add({
                     to: this.$route.params.id,
-                    from: this.user.id,
+                    from: this.user.alias,
                     content: this.newComment,
                     time: Date.now()
                 }).then(()=>{
@@ -64,3 +83,20 @@ export default {
     }
 }
 </script>
+
+<style>
+
+.view-profile .card{
+    padding: 20px;
+    margin-top: 60px;
+}
+.view-profile h2{
+    font-size: 2em;
+    margin-top: 0;
+}
+.view-profile li{
+    padding: 10px;
+    border: 1px solid #eee;
+}
+
+</style>
