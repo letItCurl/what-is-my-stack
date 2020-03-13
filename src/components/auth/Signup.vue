2a8c03ca70714db255b4c510d052c5f587f85880
rollpz <rolandlopez.developer@gmail.com>
2020-03-13 15:09:44 +0100

    Signup component

---
+++ src/components/auth/Signup.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class='signup container'>
+        <form @submit.prevent="signup" class="card-panel">
+            <h2 class="center deep-purple-text">Signup</h2>
+            <div class="field">
+                <label for="email">Email: </label>
+                <input type="email" v-model="email">
+            </div>
+            <div class="field">
+                <label for="password">Password: </label>
+                <input type="password" v-model="password">
+            </div>
+            <div class="field">
+                <label for="alias">Alias: </label>
+                <input type="text" v-model="alias">
+            </div>
+            <p class="red-text center" v-if="feedback">{{feedback}}</p>
+            <div class="field center">
+                <button class="btn deep-purple">
+                    Signup
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Signup',
+    data(){
+        return{
+            email: null,
+            password: null,
+            alias: null,
+            feedback: null
+        }
+    },
+    methods: {
+        signup(){
+            if(this.alias){
+
+            }else{
+                this.feedback = "you must enter a feedback"
+            }
+        }
+    }
+    
+}
+</script>
+
+<style>
+.signup{
+    max-width: 400px;
+    margin-top: 60px;
+}
+.signup h2{
+    font-size: 2.4em;
+}
+.signup .field{
+    margin-bottom: 16px;
+}
+</style>
---
+++ src/components/layouts/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="navbar">
+        <nav class="deep-purple darken-1">
+            <div class="container">
+                <a href="" class="brand-logo left">What is my stack !</a>
+                <ul class="right">
+                    <li><a href="">Signup</a></li>
+                    <li><a href="">Login</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+<style>
+
+</style>
