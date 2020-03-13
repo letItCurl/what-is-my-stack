6aef4b8d91fa626dd5951080c16bcbe1f3a931e5
rollpz <rolandlopez.developer@gmail.com>
2020-03-13 18:24:25 +0100

    Scaffolding comments

--- src/components/home/GMap.vue
+++ src/components/home/GMap.vue
@@ -6,6 +6,7 @@
 
 <script>
 import firebase from 'firebase'
+import db from '@/firebase/init'
 export default {
     name: 'Gmap',
     data(){
@@ -23,14 +24,47 @@ export default {
                 minZoom: 3,
                 streetViewControl: false
             })
+
+            db.collection('users').get().then(users =>{
+                users.docs.forEach(doc =>{
+                    let data = doc.data()
+                    console.log(data.geolocation.lat,data.geolocation.lng)
+                    if(data.geolocation){
+                        let marker = new google.maps.Marker({
+                            position: {
+                                lat: data.geolocation.lat,
+                                lng: data.geolocation.lng
+                            }
+                        })
+                        marker.setMap(map);
+                        marker.addListener('click', () => {
+                            this.$router.push({name: 'ViewProfile', params: {id: doc.id}})
+                        })
+                    }
+                })
+            })
         }
     },
     mounted(){
+        let user = firebase.auth().currentUser
         if(navigator.geolocation){
             navigator.geolocation.getCurrentPosition((pos) => {
                 this.lat = pos.coords.latitude
                 this.lng = pos.coords.longitude
-                this.renderMap()
+                // update user's coords
+                db.collection('users').where('user_id', '==', user.uid)
+                .get().then(snapshot => {
+                    snapshot.forEach((doc) => {
+                        db.collection('users').doc(doc.id).update({
+                            geolocation: {
+                                lat: pos.coords.latitude,
+                                lng: pos.coords.longitude
+                            }
+                        })
+                    })
+                }).then(() => {
+                    this.renderMap()
+                })
             }
             ,(e) => {console.log(e); this.renderMap()}
             ,{maximumAge: 60000, timeout: 3000})
--- src/router/index.js
+++ src/router/index.js
@@ -3,15 +3,20 @@ import Router from 'vue-router'
 import GMap from '@/components/home/GMap'
 import Signup from '@/components/auth/Signup'
 import Login from '@/components/auth/Login'
+import ViewProfile from '@/components/profile/ViewProfile'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+ const router = new Router({
   routes: [
     {
       path: '/',
       name: 'GMap',
-      component: GMap
+      component: GMap,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/signup',
@@ -22,6 +27,29 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login
+    },
+    {
+      path: '/profile/:id',
+      name: 'ViewProfile',
+      component: ViewProfile,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach((to,from,next) => {
+  if(to.matched.some(rec => rec.meta.requiresAuth)){
+    let user = firebase.auth().currentUser
+    if(user){
+      next()
+    }else{
+      next({name: 'Login'})
+    }
+  }else{
+    next()
+  }
+})
+
+export default router
